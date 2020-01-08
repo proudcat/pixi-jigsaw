a239d6db0bb6d00dfca8f66d62d5f7a73d6528e6
yulijun <koalaylj@gmail.com>
2020-01-08 23:01:22 +0800

    优化构建脚本

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,13 @@ const {
   parallel
 } = require('gulp')
 
-const fs = require('fs')
 const del = require('del')
 const argv = require('yargs').argv
 const path = require('path')
 const gulpif = require('gulp-if')
 const imagemin = require('gulp-imagemin')
 const webpack = require('webpack')
-const MemoryFS = require('memory-fs')
-const options = require('./webpack.config')
+const webpack_config = require('./webpack.config')
 
 function clean(next) {
   del.sync('dist')
@@ -37,37 +35,16 @@ function copyAssets() {
 }
 
 function jsBundle(next) {
+  webpack_config.mode = argv.mode
 
-  options.mode = argv.mode
-
-  const compiler = webpack(options)
-
-  let memory = new MemoryFS()
-  compiler.outputFileSystem = memory
+  const compiler = webpack(webpack_config)
 
   compiler.run((err, stats) => {
     if (err || stats.hasErrors()) {
       console.error(stats.toJson().errors)
     }
-    let dest = path.join(options.output.path, options.output.filename)
-    const bundle = memory.readFileSync(dest, 'utf-8')
-    const js = concat(bundle)
-    fs.writeFileSync(dest, js, 'utf8')
     next()
   })
-}
-
-function concat(bundle) {
-  return bundle
-  let libs = ['src/lib/pixi-sound.js']
-  let js = ''
-  libs.forEach(lib => {
-    js += fs.readFileSync(lib, 'utf8')
-    js += ';\n'
-  })
-  js += bundle
-
-  return js
 }
 
 exports.dist = series(clean, parallel(copyAssets, jsBundle))
