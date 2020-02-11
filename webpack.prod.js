1ab7d9b0c004faf7de46ca2a7f65ad1d91b8dd56
yulijun <koalaylj@gmail.com>
2020-02-11 17:36:27 +0800

    remove quote of key

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  'mode':'production',
+  mode:'production',
   devtool: 'source-map',
   module: {
     rules: [{
